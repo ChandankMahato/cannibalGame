cadec99cf17485abd36cedc33a9392b741eccc4a
Chandan <cmahato2000@gmail.com>
2023-09-23 14:13:54 +0545

    bug fixed

--- UI/script.js
+++ UI/script.js
@@ -2,7 +2,23 @@
     var boatLeft = document.querySelector(".boat-left");
     var boatRight = document.querySelector(".boat-right");
 
-    var AllState = [[0,2,-1],[0,1,1],[0,2,-1],[0,1,1],[2,0,-1],[1,1,1],[2,0,-1],[0,1,1],[0,2,-1],[0,1,1],[0,2,-1],];
+    var initialState = [3,3,1];
+
+    var solution = [[0,2,-1],[0,1,1],[0,2,-1],[0,1,1],[2,0,-1],[1,1,1],[2,0,-1],[0,1,1],[0,2,-1],[0,1,1],[0,2,-1],];
+    var solutionStates = [initialState];
+    for(state of solution){
+      let nextState = [0,0,0];
+      if(state[2] === -1){
+        nextState[0] = solutionStates[solutionStates.length-1][0] - state[0]
+        nextState[1] = solutionStates[solutionStates.length-1][1] - state[1]
+        nextState[2] = 0
+      }else{
+        nextState[0] = solutionStates[solutionStates.length-1][0] + state[0]
+        nextState[1] = solutionStates[solutionStates.length-1][1] + state[1]
+        nextState[2] = 1;
+      }
+      solutionStates.push(nextState);
+    }
 
     function resetStyles() {
       boatLeft.style.display = "";
@@ -22,11 +38,11 @@
     }
 
     function processNextState(index) {
-      if (index >= AllState.length) {
+      if (index >= solutionStates.length) {
         return;
       }
 
-      var currentState = AllState[index];
+      var currentState = solutionStates[index];
 
       if (JSON.stringify(currentState) === JSON.stringify([3, 3, 1])) {
         document.getElementById("wooden-plank-1").style.marginTop = "25px";
@@ -39,6 +55,7 @@
         for (let i = 1; i <= 3; i++) {
           var devilRight = document.querySelector(`.devil-right-${i}`);
           devilRight.style.display = "none";
+
           var manRight = document.querySelector(`.man-right-${i}`);
           manRight.style.display = "none";
         }
@@ -46,6 +63,7 @@
         for (let i = 1; i <= 3; i++) {
           var devilLeft = document.querySelector(`.devil-left-${i}`);
           devilLeft.style.display = "";
+
           var manLeft = document.querySelector(`.man-left-${i}`);
           manLeft.style.display = "";
         }
@@ -59,29 +77,31 @@
           boatRight.style.display = "none";
         }
 
-        let devilCount = 3 - currentState[0];
+        let devilCount = 3 - currentState[1];
         for (let i = 1; i <= devilCount; i++) {
           var devilElement = document.querySelector(`.devil-right-${i}`);
           devilElement.style.display = "none";
         }
-        for (let i = 1; i <= currentState[0]; i++) {
+
+        for (let i = 1; i <= currentState[1]; i++) {
           var devilElement = document.querySelector(`.devil-left-${i}`);
           devilElement.style.display = "none";
         }
-        let manCount = 3 - currentState[1];
+
+        let manCount = 3 - currentState[0];
         for (let i = 1; i <= manCount; i++) {
           var manElement = document.querySelector(`.man-right-${i}`);
           manElement.style.display = "none";
         }
-
-        for (let i = 1; i <= currentState[1]; i++) {
+        for (let i = 1; i <= currentState[0]; i++) {
           var manElement = document.querySelector(`.man-left-${i}`);
           manElement.style.display = "none";
         }
       }
 
-      document.getElementById("state-value").innerHTML = AllState[index];
-      if (index < AllState.length - 1) {
+      document.getElementById("state-value").innerHTML = solutionStates[index];
+      document.getElementById("step-value").innerHTML = (index-1) === -1 ? [0,0,0] : solution[index-1];
+      if (index < solutionStates.length - 1) {
         // Check if not the last iteration
         setTimeout(function () {
           resetStyles();
@@ -91,4 +111,5 @@
     }
 
     processNextState(0);
+  
   
